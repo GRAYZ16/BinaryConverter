889a15f98db87a56f0f3db7420ea2ae7d520cdd6
Joshua Gray <joshua.gray2@my.jcu.edu.au>
2019-03-26 22:18:14 +1000

    Basic Main Activity Design

---
+++ app/src/main/java/au/edu/jcu/cp3406/binaryconverter/ConverterActivity.java
@@ -0,0 +1,15 @@
+package au.edu.jcu.cp3406.binaryconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConverterActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_converter);
+    }
+}
